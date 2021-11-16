5e6b0631601ba562753a592694c4cc93f1c2c7f5
Chalie Choi <ddd1331@gmail.com>
2021-11-16 20:55:00 +0900

    feature: 메인 팝업 특징

---
+++ src/components/Characteristics.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <div style="text-align: center;">
+      <v-chip v-for="(feature, index) in features" :key="index"
+        class="ma-2"
+        color="success"
+        outlined
+      >
+        💰
+        {{feature.title}}
+      </v-chip>
+    </div>
+    <div>
+      <G-Chart
+        type="ColumnChart"
+        :data="chartData"
+        :options="chartOptions"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import { GChart } from 'vue-google-charts';
+
+export default {
+  components: { GChart },
+  data() {
+    return {
+      chartData: [
+        ['ㅅㄷㄴㅅ', 'sㅅㄷㄴㅅ'],
+        ['1', 4],
+        ['2', 3],
+        ['3', 31],
+        ['4', 10],
+        ['5 ~', 10],
+      ],
+      chartOptions: {
+        title: '경력?',
+        hAxis: { title: '연' },
+        vAxis: { title: '명' },
+        height: 500,
+        colors: ['#ff7f32'],
+      },
+      features: [
+        { icon: '💰', title: 'test', number: 55 },
+        { icon: '💰', title: 'Conservative politics', number: 55 },
+        { icon: '💰', title: 'test', number: 55 },
+        { icon: '💰', title: 'test', number: 55 },
+        { icon: '💰', title: 'crossfit', number: 55 },
+        { icon: '💰', title: 'test', number: 55 },
+        { icon: '💰', title: 'test', number: 55 },
+        { icon: '💰', title: 'test', number: 55 },
+        { icon: '💰', title: 'test', number: 55 },
+        { icon: '💰', title: 'Conservative politics', number: 55 },
+        { icon: '💰', title: 'test', number: 55 },
+        { icon: '💰', title: 'test', number: 55 },
+        { icon: '💰', title: 'crossfit', number: 55 },
+        { icon: '💰', title: 'test', number: 55 },
+        { icon: '💰', title: 'test', number: 55 },
+        { icon: '💰', title: 'test', number: 55 },
+        { icon: '💰', title: 'Conservative politics', number: 55 },
+        { icon: '💰', title: 'test', number: 55 },
+        { icon: '💰', title: 'crossfit', number: 55 },
+        { icon: '💰', title: 'test', number: 55 },
+        { icon: '💰', title: 'crossfit', number: 55 },
+        { icon: '💰', title: 'test', number: 55 },
+        { icon: '💰', title: 'Conservative politics', number: 55 },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/components/WeatherTable.vue
@@ -0,0 +1,108 @@
+<template>
+    <v-simple-table >
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th class="text-left"></th>
+            <th class="text-left">1월</th>
+            <th class="text-left">2월</th>
+            <th class="text-left">3월</th>
+            <th class="text-left">4월</th>
+            <th class="text-left">5월</th>
+            <th class="text-left">6월</th>
+            <th class="text-left">7월</th>
+            <th class="text-left">8월</th>
+            <th class="text-left">9월</th>
+            <th class="text-left">10월</th>
+            <th class="text-left"
+              style="border-left: solid black;
+              border-right: solid black; border-top:  solid black; ">11월</th>
+            <th class="text-left">12월</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="(item, index) in desserts"
+            :key="item.name"
+          >
+            <td>{{ item.title }}</td>
+            <td :style="getColor(item.calories)">{{ item.calories }}</td>
+            <td :style="getColor(item.calories)">{{ item.calories }}</td>
+            <td :style="getColor(item.calories)">{{ item.calories }}</td>
+            <td :style="getColor(item.calories)">{{ item.calories }}</td>
+            <td :style="getColor(item.calories)">{{ item.calories }}</td>
+            <td :style="getColor(item.calories)">{{ item.calories }}</td>
+            <td :style="getColor(item.calories)">{{ item.calories }}</td>
+            <td :style="getColor(item.calories)">{{ item.calories }}</td>
+            <td :style="getColor(item.calories)">{{ item.calories }}</td>
+            <td :style="getColor(item.calories)">{{ item.calories }}</td>
+            <td :style="getColor(item.calories) + getBorder(index)">{{ item.calories }}</td>
+            <td :style="getColor(item.calories)">{{ item.calories }}</td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+</template>
+<script>
+export default {
+  methods: {
+    getColor(calories) {
+      if (calories > 500) return 'background-color: tomato;';
+      if (calories > 400) return 'background-color: orange;';
+      if (calories > 300) return 'background-color: powderblue;';
+      if (calories > 200) return 'background-color: yellow;';
+      return 'background-color: grey;';
+    },
+    getBorder(index) {
+      const isLast = index === this.desserts.length - 1;
+      return `border-left: solid; border-right: solid; ${isLast ? 'border-bottom: solid;' : ''}`;
+    },
+  },
+  data() {
+    return {
+      desserts: [
+        {
+          title: '체감',
+          name: 'Frozen Yogurt',
+          calories: 159,
+        },
+        {
+          title: '습도',
+          name: 'Ice cream sandwich',
+          calories: 262,
+        },
+        {
+          title: '강수',
+          name: 'Eclair',
+          calories: 305,
+        },
+        {
+          title: '대기질',
+          name: 'Cupcake',
+          calories: 237,
+        },
+        {
+          title: '자외선',
+          name: 'Gingerbread',
+          calories: 375,
+        },
+        {
+          title: '실제',
+          name: 'Jelly bean',
+          calories: 356,
+        },
+        {
+          title: '구름',
+          name: 'Lollipop',
+          calories: 392,
+        },
+        {
+          title: '호보스',
+          name: 'Honeycomb',
+          calories: 452,
+        },
+      ],
+    };
+  },
+};
+</script>
