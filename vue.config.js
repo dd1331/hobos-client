c187d9524943c487fa9a3c0752bfbb1d329fd269
Chalie Choi <ddd1331@gmail.com>
2022-01-10 22:50:39 +0900

    setting: add environment variables to vue.config

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,6 @@ module.exports = {
   transpileDependencies: [
     'vuetify',
   ],
+  VUE_APP_LOCAL_SERVICE: process.env.VUE_APP_LOCAL_SERVICE,
+  VUE_APP_SERVER_HOST: process.env.VUE_APP_SERVER_HOST,
 };
