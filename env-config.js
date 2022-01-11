01344fc333a566caa6754d8345eb302547eafee0
Chalie Choi <ddd1331@gmail.com>
2022-01-11 11:20:21 +0900

    setting: hard code service url for now

--- env-config.js
+++ env-config.js
@@ -1,2 +1,5 @@
-export const VUE_APP_SERVER_HOST = process.env.VUE_APP_SERVER_HOST ? process.env.VUE_APP_SERVER_HOST : 'http://localhost:3000';
-export const VUE_APP_LOCAL_SERVICE = process.env.VUE_APP_LOCAL_SERVICE ? process.env.VUE_APP_LOCAL_SERVICE : 'http://localhost:4000';
+// TODO move to env file
+export const VUE_APP_SERVER_HOST = process.env.VUE_APP_SERVER_HOST
+  ? process.env.VUE_APP_SERVER_HOST : 'http://hobos-load-balancer-cec68ef9f5418048.elb.ap-northeast-2.amazonaws.com:3000';
+export const VUE_APP_LOCAL_SERVICE = process.env.VUE_APP_LOCAL_SERVICE
+  ? process.env.VUE_APP_LOCAL_SERVICE : 'http://hobos-load-balancer-cec68ef9f5418048.elb.ap-northeast-2.amazonaws.com:4000';
